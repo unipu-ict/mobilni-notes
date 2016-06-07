fe0aab2d5cfb7293c369015d459d65797f40996b
Siniša Štimac <sstimac@unipu.hr>
2016-06-07 21:07:09 +0200

    Aktivnost za uredjivanje biljeski, floatingbar vodi na nju

--- app/src/main/java/hr/unipu/mobilne/notes/MainActivity.java
+++ app/src/main/java/hr/unipu/mobilne/notes/MainActivity.java
@@ -1,5 +1,6 @@
 package hr.unipu.mobilne.notes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -19,11 +20,15 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, UredjivanjeActivity.class);
+                startActivity(intent);
+
             }
         });
     }
